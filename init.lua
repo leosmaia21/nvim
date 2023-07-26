0726cffdb90b70b1a4a09abb473dcac21d3a439c
leosmaia21 <leosmaia21@gmail.com>
2023-07-26 15:59:26 +0100

    maravilha

--- init.lua
+++ init.lua
@@ -73,14 +73,9 @@ palette_overrides = {
 
 vim.g.gruvbox_material_background = "hard"
 vim.g.gruvbox_material_better_performance = 1
+vim.g.gruvbox_material_foreground = 'original'
 vim.g.gruvbox_material_colors_override ={
 	 bg0 = {'#181919', '255'},
-	 fg0 = {'#ebdbb2', '255'},
-	 red = {'#fb4934', '255'},
-	 green = {'#b8bb26', '255'},
-	 yellow = {'#fabd2f', '255'},
-	 orange = {'#fe8019', '255'}
-	 
 }
 vim.cmd.colorscheme('gruvbox-material')
 
@@ -133,50 +128,40 @@ require("lualine").setup{
 		lualine_a = {function()
 			local currentFile = vim.fn.split(vim.api.nvim_buf_get_name(0), "/")
 			currentFile = currentFile[#currentFile]
-			if vim.api.nvim_buf_get_option(0, 'buftype') == '' then
-				local lenSize = string.len(currentFile)
-				if (lenSize > maxSizeFile) then maxSizeFile = lenSize end
-				if (maxSizeFile % 2 == 0) then maxSizeFile = maxSizeFile + 1 end
-				local padding = math.floor((maxSizeFile - lenSize) / 2)
-				if lenSize % 2 == 0 then currentFile = currentFile .. " " end
-				padding = string.rep(" ", padding)
-				return padding .. currentFile .. padding
-			else
-				return currentFile
-			end
+			if vim.api.nvim_buf_get_option(0, 'buftype') ~= '' then return currentFile end
+			local lenSize = string.len(currentFile)
+			if (lenSize > maxSizeFile) then maxSizeFile = lenSize end
+			local padding = math.floor((maxSizeFile - lenSize) / 2)
+			if lenSize % 2 == 0 then currentFile = currentFile .. " " end
+			padding = string.rep(" ", padding)
+			return padding .. currentFile .. padding
 			end,
 			function()
-				if vim.api.nvim_buf_get_option(0, 'buftype') == ''  then
-					for i, buf in ipairs(vim.api.nvim_list_bufs()) do
-						if vim.api.nvim_buf_get_option(buf, 'buftype') == ''  then
-							if vim.api.nvim_buf_get_option(buf,'modified') then
-								return 'Unsaved files' -- any message or icon
-							end
+				if vim.api.nvim_buf_get_option(0, 'buftype') ~= ''  then return '' end
+				for i, buf in ipairs(vim.api.nvim_list_bufs()) do
+					if vim.api.nvim_buf_get_option(buf, 'buftype') == ''  then
+						if vim.api.nvim_buf_get_option(buf,'modified') then
+							return 'Unsaved files' -- any message or icon
 						end
 					end
-					return ''
-				else
-					return ''
 				end
+				return ''
 			end,
 		},
 		lualine_b = {{ 'diagnostics', sources = { 'nvim_lsp' }}, 
-			function() 
-				if vim.api.nvim_buf_get_option(0, 'buftype') == ''  then
-					local tabela = require("harpoon").get_mark_config()['marks']
-					local ret = ""
-					local currentFile = vim.fn.split(vim.api.nvim_buf_get_name(0), "/")
-					currentFile = currentFile[#currentFile]
-					for key, value in pairs(tabela) do
-						file = vim.fn.split(value['filename'], "/")
-						file = file[#file]
-						if file == currentFile then file = file .. "*" else file = file .. " " end 
-						ret = ret .. "  " ..  key .. " " .. file
-					end
-					return ret
-				else
-					return ''
+		function() 
+				if vim.api.nvim_buf_get_option(0, 'buftype') ~= '' then return '' end
+				local tabela = require("harpoon").get_mark_config()['marks']
+				local currentFile = vim.fn.split(vim.api.nvim_buf_get_name(0), "/")
+				currentFile = currentFile[#currentFile]
+				local ret = {}
+				for key, value in pairs(tabela) do
+					local file = vim.fn.split(value['filename'], "/")
+					file = file[#file]
+					file = file == currentFile and file .. "*" or file .. " "
+					table.insert(ret, "  " .. key .. " " .. file)
 				end
+				return table.concat(ret)
 		end},
 		lualine_c = {},
 		lualine_x = {},
