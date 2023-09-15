d9e2fd7ae600247fcb33e42bc47b7cae291f4c28
leosmaia21 <leosmaia21@gmail.com>
2023-09-15 20:27:37 +0100

    indent from treesitter

--- init.lua
+++ init.lua
@@ -53,7 +53,8 @@ require("lazy").setup({
 		require'nvim-treesitter.configs'.setup {
 			ensure_installed = {"vim", "lua", "c", "python" },
 			auto_install = true,
-			highlight = {enable = true, indent = true, additional_vim_regex_highlighting = false},
+			highlight = {enable = true, additional_vim_regex_highlighting = false},
+			indent = { enable = true },  
 		}
 	end
 	},
